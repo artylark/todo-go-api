e5cc0e56df3c9155840a947389d83e3d4c70c6d6
Shun Kashima <x.world.dive@gmail.com>
2024-06-27 23:22:44 +0900

    feat: enable to get todos from database

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/artylark/todo-go-api/infrastructure"
 	"github.com/labstack/echo/v4"
-	"net/http"
+	"time"
 )
+
+type Todo struct {
+	Id        int       `gorm:"primary_key" json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Todos []Todo
+
+func (Todo) TableName() string {
+	return "todo"
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "API called")
-	})
 
 	db := infrastructure.Connect()
-	fmt.Println(db)
+	e.GET("/todo/get", func(c echo.Context) error {
+		todos := Todos{}
+		db.Find(&todos)
+		return json.NewEncoder(c.Response()).Encode(todos)
+	})
+	e.GET("/todo/get/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		todo := Todo{}
+		db.Where("Id = ?", id).First(&todo)
+		return json.NewEncoder(c.Response()).Encode(todo)
+	})
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
